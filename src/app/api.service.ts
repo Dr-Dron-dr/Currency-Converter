35292e4075fe016c3f966b5b6ea0fc7dc3cd62b4
Dr-Dron-dr <ponomarenkodr@gmail.com>
2023-02-27 20:35:28 +0200

    changes in the index.html

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export type ApiInfo = {
   rates: {
     [key: string]: number;
@@ -11,9 +13,10 @@ export type ApiInfo = {
   providedIn: 'root',
 })
 export class ApiService {
-  private cacheTime = 3600; 
+  private cacheTime = 3600;
   private cache: { [key: string]: { timestamp: number; data: ApiInfo } } = {};
-  private apiUrl = 'https://openexchangerates.org/api/latest.json?app_id=db430cc8f8224f8db942276748be4bee';
+  private apiUrl =
+    'https://openexchangerates.org/api/latest.json?app_id=${process.env.API_KEY}';
 
   async fetchData(): Promise<ApiInfo> {
     const now = Date.now();

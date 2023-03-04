1a45e39e22b6ca81aff55f78d515433be2ebdad5
Dr-Dron-dr <ponomarenkodr@gmail.com>
2023-03-04 10:45:38 +0200

    Your commit message here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,7 +9,7 @@ import { CurrencyConverterComponent } from './currency-converter/currency-conver
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, CurrencyConverterComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
